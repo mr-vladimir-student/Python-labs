1f2138eb13f122a8064ab21f5eb1b1da9d36eda6
mr-vladimir-student <144148195+mr-vladimir-student@users.noreply.github.com>
2023-09-05 17:23:18 +0200

    Update firstLabCode.py
    Added All Three tasks to the file

--- firstLabCode.py
+++ firstLabCode.py
@@ -1,3 +1,37 @@
+# First Task Code
+
+# Prompt to enter positive numbers
+a = float(input("Enter a positive value for a: "))
+b = float(input("Enter a positive value for b: "))
+
+# Check if a and b are positive
+if a <= 0 or b <= 0:
+    print("Both a and b must be positive numbers.")
+else:
+    # Calculating x
+    if a > b:
+        x = a * b + 31
+    elif a < b:
+        
+        x = (a * 5 - 1) / b
+    else:
+        x = -25
+    
+    # Displaying the result
+    print(f"The value of 'X' is: {x}")
+
+
+# Second Task Code
+
+print(" x\t    y")
+for i in range(0, 9):
+    x = i/2
+    y = x 
+    print(f"{x:.1f}\t   {y:.1f}")
+
+
+# Third Task Code
+
 # Prompt to enter a number for the rows and columns
 n = int(input("Enter a number between 1 and 9: "))
 print(" ")
